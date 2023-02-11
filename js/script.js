f811be0de24575b4983e55233660c33f6b2181dc
Vlad <vladislav.stan.1998@gmail.com>
2023-02-11 11:43:26 +0200

    added the ability to add items to the list, the ability to delete and sort them.

--- js/script.js
+++ js/script.js
@@ -1,72 +1,103 @@
-/* Задания на урок:
-
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
-
-2) Изменить жанр фильма, поменять "комедия" на "драма"
-
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
-
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
-
-5) Добавить нумерацию выведенных фильмов */
-
 'use strict';
 
-const promoAdv = document.querySelectorAll('.promo__adv img'),
+document.addEventListener('DOMContentLoaded', () => {
+
+    const promoAdv = document.querySelectorAll('.promo__adv img'),
       promoBg = document.querySelector('.promo__bg'),
       promoGenre = promoBg.querySelector('.promo__genre'),
-      header = document.querySelector('header'),
-      promoInteractiveList = document.querySelector('.promo__interactive-list');
-
-
-const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
-};
-
-
-function removingAd() {
-    promoAdv.forEach(elem => {
-        elem.remove();
-    });
-}
-
-removingAd();
-
-
-function сhangeGenre() {
-    promoGenre.textContent = 'ДРАМА';
-}
-
-сhangeGenre();
-
-
-function bgImg() {
-    promoBg.style.cssText = 'background-image: url("img/bg.jpg")';
-}
-
-bgImg();
-
-
-function changeViwedMovies({movies}) {
-    promoInteractiveList.innerHTML = '';
+      promoInteractiveList = document.querySelector('.promo__interactive-list'),
+      form = document.querySelector('form.add'),
+      inputForm = form.querySelector('.adding__input'),
+      checkbox = form.querySelector('[type="checkbox"]');
+      
     
-    movies.sort();
-    
-    movies.forEach((elem, i) => {
-        promoInteractiveList.innerHTML += `
-            <li class="promo__interactive-item">${i + 1}. ${elem}
-                <div class="delete"></div>
-            </li>
-        `;
-    });
-}
+    const movieDB = {
+        movies: [
+            "Логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против..."
+        ]
+    };
 
-changeViwedMovies(movieDB);
+
+    const removingAdv = (ad) => {
+        ad.forEach(elem => {
+            elem.remove();
+        });
+    };
+
+
+    const makeChanges = (genre, promoBag) => {
+        genre.textContent = 'ДРАМА';
+        promoBag.style.cssText = 'background-image: url("img/bg.jpg")';
+    };
+
+
+    const sortArr = (films) => {
+        films.sort();
+    };
+
+
+    function addNewFilms() {
+        form.addEventListener('submit', (event) => {
+            event.preventDefault();
+
+            let newFilm = inputForm.value;
+            const favorite = checkbox.checked;
+
+
+            if (newFilm) {
+
+                if (newFilm.length > 21) {
+                    newFilm = `${newFilm.substr(0, 21)}...`;
+                } 
+
+                if (favorite) {
+                    console.log('Your film added to favorite');
+                }
+
+                movieDB.movies.push(newFilm);  
+                sortArr(movieDB.movies);
+                changeViwedMovies(movieDB.movies, promoInteractiveList);
+                event.target.reset();
+            } 
+        });
+    }
+
+
+    function changeViwedMovies(films, parent) {
+        parent.innerHTML = '';
+        sortArr(films);
+
+        films.forEach((elem, i) => {
+            parent.innerHTML += `
+                <li class="promo__interactive-item">${i + 1}. ${elem}
+                    <div class="delete"></div>
+                </li>
+            `;
+        });
+
+        document.querySelectorAll('.delete').forEach((item, i) => {
+            item.addEventListener('click', () => {
+                item.parentElement.remove();
+                movieDB.movies.splice(i, 1);
+
+                changeViwedMovies(films, parent);
+            });
+        });
+    }
+
+    
+    
+    
+
+    
+    removingAdv(promoAdv);
+    makeChanges(promoGenre, promoBg);
+    changeViwedMovies(movieDB.movies, promoInteractiveList);
+    addNewFilms();
+
+
+});
